d7f20ef4f8127c0eddbfbd697abbe3cc551ee37b
Shawn Harness <rsharness@gmail.com>
2024-11-30 20:44:51 -0600

    Pre Jenkins Tie in

--- src/main/java/net/judgelan/radio365/controllers/radio.java
+++ src/main/java/net/judgelan/radio365/controllers/radio.java
@@ -1,21 +1,30 @@
 package net.judgelan.radio365.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-public class radio {
 
+@Controller
+//@RequestMapping
+public class radio {
 
     public String myName = "myName";
 
     public String yourName  = "yourName";
 
-@GetMapping
-@ResponseBody
-    public String hello(){
-        return "Hello" + yourName;
+    @GetMapping("/")
+    public String welcomeMessage(){
+        return "Hello, Guy";
+    }
+
+    @GetMapping("form")
+    @ResponseBody
+    public String hello(Model model){
+        model.addAttribute();
+        return "form";
     }
 
 
