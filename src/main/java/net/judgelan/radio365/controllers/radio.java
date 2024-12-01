96e0aa0793674bb40bdfe96fd9d7c8692f26dae7
Shawn Harness <rsharness@gmail.com>
2024-12-01 17:34:15 -0600

    Dev branch changes to merge into main

--- src/main/java/net/judgelan/radio365/controllers/radio.java
+++ src/main/java/net/judgelan/radio365/controllers/radio.java
@@ -2,6 +2,7 @@ package net.judgelan.radio365.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,12 +12,31 @@ public class radio {
     public String myName = "myName";
 
     public String yourName  = "yourName";
-
-@GetMapping
-@ResponseBody
+    //
+    @GetMapping("hi")
+    @ResponseBody
     public String hello(){
-        return "Hello" + yourName;
+        return "Hello " + yourName;
     }
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloThere(@RequestParam String name){
+        return "Hello " + name;
+    }
+//
+    @GetMapping("bye")
+    @ResponseBody
+    public String bye(){
+        return "aufwiedersehen " + yourName;
+    }
+
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String latez(@RequestParam String name){
+        return "latez, bruh " + name;
+    }
+//
+
 
 
 }
