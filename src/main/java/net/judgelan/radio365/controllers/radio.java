479917c234b6831a1efcd83ae340e1d71b8b3569
Shawn Harness <rsharness@gmail.com>
2024-12-08 21:39:06 -0600

    heiarchy restructure back to baseline

--- src/main/java/net/judgelan/radio365/controllers/radio.java
+++ src/main/java/net/judgelan/radio365/controllers/radio.java
@@ -4,33 +4,44 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 public class radio {
     // FRONTPAGE
     @GetMapping("/")
-    public String homePage(){
+    public String homePage(Model model){
+        model.addAttribute("headLine", "Test Title");
+        model.addAttribute("infoPane", "This Page is a test");
         String html = "index";
         return html;
     }
 
-    //  FORM
-    @RequestMapping(value = "form", method = {RequestMethod.GET, RequestMethod.POST})
-    public String hello(@RequestParam String formFirstName, @RequestParam String formLastName, Model model) {
-        String inputFormFirstname = "firstName";
-        String inputFormLastName = "lastName";
-        model.addAttribute("firstName", inputFormLastName);
-        model.addAttribute("lastName", inputFormLastName);
-
-        return "html/form";
+        //  FORM
+        @RequestMapping(value = "form", method = {RequestMethod.GET, RequestMethod.POST})
+            public String hello(Model model) {
+            return "html/form";
     }
 
-        @GetMapping("form")
-        @ResponseBody
-            public String howdy(){
-                String hola = "dklfajslkdfjalksdjf";
-                return hola;
-        }
 
+
+        @GetMapping("list")
+        public String namesList(Model model){
+            List<String> listData = new ArrayList<>();
+                listData.add("Me");
+                listData.add("you");
+                listData.add("he");
+                listData.add("she");
+                listData.add("it");
+                listData.add("we");
+                listData.add("they");
+
+                model.addAttribute("names", listData );
+
+            return "html/listDataPage";
+
+        }
 
 }
